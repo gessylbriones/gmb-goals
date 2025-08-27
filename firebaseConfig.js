b733a14c5a00dcea10a83cdc9e308576eb4ae74c
janjan3124 <janberalisoso19@gmail.com>
2025-08-27 09:46:27 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCchtdt34y_cL9rAOq8BGOQsrPdZ5UGivs",
+  authDomain: "gmb-goals.firebaseapp.com",
+  projectId: "gmb-goals",
+  storageBucket: "gmb-goals.firebasestorage.app",
+  messagingSenderId: "497252513277",
+  appId: "1:497252513277:web:ccea3fe44b00f1ac621594"
+};
+
+const app = initializeApp(firebaseConfig);
